fcd6226120494cd9240ec7465923da9fea39e966
Lionel Touati <ltouati@gmail.com>
2019-01-05 10:16:33 +0100

    [PERF] - Use native client size
    Use clientWidth & clientHeight that seem to give much better performance on chrome when moving very large graphs

--- src/svg.panzoom.js
+++ src/svg.panzoom.js
@@ -165,13 +165,12 @@ SVG.extend(SVG.Doc, SVG.Nested, {
   },
 
   zoom: function(level, point) {
-    var style = window.getComputedStyle(this.node)
-      , width = parseFloat(style.getPropertyValue('width'))
-      , height = parseFloat(style.getPropertyValue('height'))
+    var width = this.node.clientWidth
+      , height = this.node.clientHeight
       , v = this.viewbox()
       , zoomX = width / v.width
       , zoomY = height / v.height
-      , zoom = Math.min(zoomX, zoomY)
+      , zoom = Math.min(zoomX, zoomY);
 
     if(level == null) {
       return zoom
