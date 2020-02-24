f97f1fa295d1b2f3bac500e90a31500a1589faeb
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2020-02-24 13:40:10 +0100

    add typings for new options

--- svg.panzoom.js.d.ts
+++ svg.panzoom.js.d.ts
@@ -1,4 +1,28 @@
+enum mousebutton {
+  left = 0,
+  middle,
+  right,
+  back,
+  forth
+}
+
+interface marginOptions {
+  left: number
+  top: number
+  right: number
+  bottom: number
+}
+
 interface options {
+  zoomFactor?: number
+  zoomMin?: number
+  zoomMax?: number
+  doPanning?: boolean
+  doPinchZoom?: boolean
+  doWheelZoom?: boolean
+  panMouse?: mousebutton
+  oneFingerPan?: boolean
+  margins?: boolean | marginOptions
   zoomFactor?: number
   zoomMin?: number
   zoomMax?: number
