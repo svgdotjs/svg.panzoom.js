43d592a272636b0538c662fff5abc2cd11924ed2
郭之存 <guoplc@163.com>
2020-05-06 16:05:04 +0800

    OK

--- src/svg.panzoom.js
+++ src/svg.panzoom.js
@@ -1,61 +1,126 @@
 import { Svg, on, off, extend, Matrix, Box } from '@svgdotjs/svg.js'
 
-const IS_SUPPORTED_TOUCH = (function hasTouchSupport() {
-  return (
-    'ontouchstart' in window || // touch events
-    (window.Modernizr && window.Modernizr.touch) || // modernizr
-    (navigator.msMaxTouchPoints || navigator.maxTouchPoints) > 2
-  ) // pointer events
-})()
-
-const EVENT_MAP = {
-  mousedown: IS_SUPPORTED_TOUCH ? 'touchstart' : 'mousedown',
-  mousemove: IS_SUPPORTED_TOUCH ? 'touchmove' : 'mousemove',
-  mouseup: IS_SUPPORTED_TOUCH ? 'touchend' : 'mouseup'
-}
-
 const normalizeEvent = (ev) =>
   ev.touches || [{ clientX: ev.clientX, clientY: ev.clientY }]
 
 extend(Svg, {
-  panZoom (options) {
+  panZoom(options) {
     this.off('.panZoom')
 
     // when called with false, disable panZoom
     if (options === false) return this
 
-    options = options || {}
-    const zoomFactor = options.zoomFactor || 0.03
-    const zoomMin = options.zoomMin || Number.MIN_VALUE
-    const zoomMax = options.zoomMax || Number.MAX_VALUE
+    options = options ?? {}
+    const zoomFactor = options.zoomFactor ?? 2
+    const zoomMin = options.zoomMin ?? Number.MIN_VALUE
+    const zoomMax = options.zoomMax ?? Number.MAX_VALUE
+    const doWheelZoom = options.wheelZoom ?? true
+    const doPinchZoom = options.pinchZoom ?? true
+    const doPanning = options.panning ?? true
+    const panButton = options.panButton ?? 0
+    const oneFingerPan = options.oneFingerPan ?? false
+    const margins = options.margins ?? false
+    const wheelZoomDeltaModeLinePixels =
+      options.wheelZoomDeltaModeLinePixels ?? 17
+    const wheelZoomDeltaModeScreenPixels =
+      options.wheelZoomDeltaModeScreenPixels ?? 53
 
-    let lastP; let lastTouches; let zoomInProgress = false
+    let lastP
+    let lastTouches
+    let zoomInProgress = false
 
-    var wheelZoom = function (ev) {
+    const restrictToMargins = (box) => {
+      if (!margins) return box
+      const { top, left, bottom, right } = margins
+      const zoom = this.width() / box.width
+
+      const { width, height } = this.attr(['width', 'height'])
+
+      const leftLimit = width - left / zoom
+      const rightLimit = (right - width) / zoom
+      const topLimit = height - top / zoom
+      const bottomLimit = (bottom - height) / zoom
+
+      box.x = Math.min(leftLimit, Math.max(rightLimit, box.x))
+      box.y = Math.min(topLimit, Math.max(bottomLimit, box.y))
+      return box
+    }
+
+    const wheelZoom = function (ev) {
       ev.preventDefault()
 
-      // touchpads can give ev.deltaY == 0, which skrews the lvl calculation
-      if (ev.deltaY === 0) return
+      // When wheeling on a mouse,
+      // - chrome by default uses deltaY = 53, deltaMode = 0 (pixel)
+      // - firefox by default uses deltaY = 3, deltaMode = 1 (line)
+      // - chrome and firefox on windows after configuring "One screen at a time"
+      //   use deltaY = 1, deltaMode = 2 (screen)
+      //
+      // Note that when when wheeling on a touchpad, deltaY depends on how fast
+      // you swipe, but the deltaMode is still different between the browsers.
+      //
+      // Normalize everything so that zooming speed is approximately the same in all cases
+      let normalizedPixelDeltaY
+      switch (ev.deltaMode) {
+        case 1:
+          normalizedPixelDeltaY = ev.deltaY * wheelZoomDeltaModeLinePixels
+          break
+        case 2:
+          normalizedPixelDeltaY = ev.deltaY * wheelZoomDeltaModeScreenPixels
+          break
+        default:
+          // 0 (already pixels) or new mode (avoid crashing)
+          normalizedPixelDeltaY = ev.deltaY
+          break
+      }
 
-      let lvl = this.zoom() - zoomFactor * ev.deltaY / Math.abs(ev.deltaY)
+      let lvl =
+        Math.pow(1 + zoomFactor, (-1 * normalizedPixelDeltaY) / 100) *
+        this.zoom()
       const p = this.point(ev.clientX, ev.clientY)
 
-      if (lvl > zoomMax) { lvl = zoomMax }
+      if (lvl > zoomMax) {
+        lvl = zoomMax
+      }
 
-      if (lvl < zoomMin) { lvl = zoomMin }
+      if (lvl < zoomMin) {
+        lvl = zoomMin
+      }
 
-      if (this.dispatch('zoom', { level: lvl, focus: p }).defaultPrevented) { return this }
+      if (this.dispatch('zoom', { level: lvl, focus: p }).defaultPrevented) {
+        return this
+      }
 
       this.zoom(lvl, p)
+
+      if (margins) {
+        const box = restrictToMargins(this.viewbox())
+        this.viewbox(box)
+      }
     }
 
     const pinchZoomStart = function (ev) {
       lastTouches = normalizeEvent(ev)
 
-      if (lastTouches.length < 2) return
+      // Start panning in case only one touch is found
+      if (lastTouches.length < 2) {
+        if (doPanning && oneFingerPan) {
+          panStart.call(this, ev)
+        }
+        return
+      }
+
+      // Stop panning for more than one touch
+      if (doPanning && oneFingerPan) {
+        panStop.call(this, ev)
+      }
+
+      // We call it so late, so the user is still able to scroll / reload the page via gesture
+      // In case oneFingerPan is not active
       ev.preventDefault()
 
-      if (this.dispatch('pinchZoomStart', { event: ev }).defaultPrevented) { return }
+      if (this.dispatch('pinchZoomStart', { event: ev }).defaultPrevented) {
+        return
+      }
 
       this.off('touchstart.panZoom', pinchZoomStart)
 
@@ -66,6 +131,12 @@ extend(Svg, {
 
     const pinchZoomStop = function (ev) {
       ev.preventDefault()
+
+      const currentTouches = normalizeEvent(ev)
+      if (currentTouches.length > 1) {
+        return
+      }
+
       zoomInProgress = false
 
       this.dispatch('pinchZoomEnd', { event: ev })
@@ -73,6 +144,10 @@ extend(Svg, {
       off(document, 'touchmove.panZoom', pinchZoom)
       off(document, 'touchend.panZoom', pinchZoomStop)
       this.on('touchstart.panZoom', pinchZoomStart)
+
+      if (currentTouches.length && doPanning && oneFingerPan) {
+        panStart.call(this, ev)
+      }
     }
 
     const pinchZoom = function (ev) {
@@ -83,40 +158,55 @@ extend(Svg, {
 
       // Distance Formula
       const lastDelta = Math.sqrt(
-        Math.pow(lastTouches[0].clientX - lastTouches[1].clientX, 2)
-        + Math.pow(lastTouches[0].clientY - lastTouches[1].clientY, 2)
+        Math.pow(lastTouches[0].clientX - lastTouches[1].clientX, 2) +
+          Math.pow(lastTouches[0].clientY - lastTouches[1].clientY, 2)
       )
 
       const currentDelta = Math.sqrt(
-        Math.pow(currentTouches[0].clientX - currentTouches[1].clientX, 2)
-        + Math.pow(currentTouches[0].clientY - currentTouches[1].clientY, 2)
+        Math.pow(currentTouches[0].clientX - currentTouches[1].clientX, 2) +
+          Math.pow(currentTouches[0].clientY - currentTouches[1].clientY, 2)
       )
 
       let zoomAmount = lastDelta / currentDelta
 
-      if ((zoom < zoomMin && zoomAmount > 1) || (zoom > zoomMax && zoomAmount < 1)) {
+      if (
+        (zoom < zoomMin && zoomAmount > 1) ||
+        (zoom > zoomMax && zoomAmount < 1)
+      ) {
         zoomAmount = 1
       }
 
       const currentFocus = {
-        x: currentTouches[0].clientX + 0.5 * (currentTouches[1].clientX - currentTouches[0].clientX),
-        y: currentTouches[0].clientY + 0.5 * (currentTouches[1].clientY - currentTouches[0].clientY)
+        x:
+          currentTouches[0].clientX +
+          0.5 * (currentTouches[1].clientX - currentTouches[0].clientX),
+        y:
+          currentTouches[0].clientY +
+          0.5 * (currentTouches[1].clientY - currentTouches[0].clientY),
       }
 
       const lastFocus = {
-        x: lastTouches[0].clientX + 0.5 * (lastTouches[1].clientX - lastTouches[0].clientX),
-        y: lastTouches[0].clientY + 0.5 * (lastTouches[1].clientY - lastTouches[0].clientY)
+        x:
+          lastTouches[0].clientX +
+          0.5 * (lastTouches[1].clientX - lastTouches[0].clientX),
+        y:
+          lastTouches[0].clientY +
+          0.5 * (lastTouches[1].clientY - lastTouches[0].clientY),
       }
 
       const p = this.point(currentFocus.x, currentFocus.y)
-      const focusP = this.point(2 * currentFocus.x - lastFocus.x, 2 * currentFocus.y - lastFocus.y)
+      const focusP = this.point(
+        2 * currentFocus.x - lastFocus.x,
+        2 * currentFocus.y - lastFocus.y
+      )
       const box = new Box(this.viewbox()).transform(
         new Matrix()
-          .translate(p.x, p.y)
-          .scale(zoomAmount, 0, 0)
           .translate(-focusP.x, -focusP.y)
+          .scale(zoomAmount, 0, 0)
+          .translate(p.x, p.y)
       )
 
+      restrictToMargins(box)
       this.viewbox(box)
 
       lastTouches = currentTouches
@@ -125,9 +215,16 @@ extend(Svg, {
     }
 
     const panStart = function (ev) {
+      const isMouse = ev.type.indexOf('mouse') > -1
+
+      // In case panStart is called with touch, ev.button is undefined
+      if (isMouse && ev.button !== panButton && ev.which !== panButton + 1) {
+        return
+      }
+
       ev.preventDefault()
 
-      this.off(`${EVENT_MAP.mousedown}.panZoom`, panStart)
+      this.off('mousedown.panZoom', panStart)
 
       lastTouches = normalizeEvent(ev)
 
@@ -137,16 +234,21 @@ extend(Svg, {
 
       lastP = { x: lastTouches[0].clientX, y: lastTouches[0].clientY }
 
-      on(document, `${EVENT_MAP.mousemove}.panZoom`, panning, this)
-      on(document, `${EVENT_MAP.mouseup}.panZoom`, panStop, this)
+      on(document, 'touchmove.panZoom mousemove.panZoom', panning, this, {
+        passive: false,
+      })
+
+      on(document, 'touchend.panZoom mouseup.panZoom', panStop, this, {
+        passive: false,
+      })
     }
 
     const panStop = function (ev) {
       ev.preventDefault()
 
-      off(document, `${EVENT_MAP.mousemove}.panZoom`, panning)
-      off(document, `${EVENT_MAP.mouseup}.panZoom`, panStop)
-      this.on(`${EVENT_MAP.mousedown}.panZoom`, panStart)
+      off(document, 'touchmove.panZoom mousemove.panZoom', panning)
+      off(document, 'touchend.panZoom mouseup.panZoom', panStop)
+      this.on('mousedown.panZoom', panStart)
 
       this.dispatch('panEnd', { event: ev })
     }
@@ -156,7 +258,10 @@ extend(Svg, {
 
       const currentTouches = normalizeEvent(ev)
 
-      const currentP = { x: currentTouches[0].clientX, y: currentTouches[0].clientY }
+      const currentP = {
+        x: currentTouches[0].clientX,
+        y: currentTouches[0].clientY,
+      }
 
       const p1 = this.point(currentP.x, currentP.y)
 
@@ -164,16 +269,37 @@ extend(Svg, {
 
       const deltaP = [p2.x - p1.x, p2.y - p1.y]
 
-      const box = new Box(this.viewbox()).transform(new Matrix().translate(deltaP[0], deltaP[1]))
+      if (!deltaP[0] && !deltaP[1]) {
+        return
+      }
+
+      const box = new Box(this.viewbox()).transform(
+        new Matrix().translate(deltaP[0], deltaP[1])
+      )
+
+      lastP = currentP
+
+      restrictToMargins(box)
+
+      if (this.dispatch('panning', { box, event: ev }).defaultPrevented) {
+        return
+      }
 
       this.viewbox(box)
-      lastP = currentP
     }
 
-    this.on('wheel.panZoom', wheelZoom)
-    this.on('touchstart.panZoom', pinchZoomStart, this, { passive: false })
-    this.on(`${EVENT_MAP.mousedown}.panZoom`, panStart, this)
+    if (doWheelZoom) {
+      this.on('wheel.panZoom', wheelZoom, this, { passive: false })
+    }
+
+    if (doPinchZoom) {
+      this.on('touchstart.panZoom', pinchZoomStart, this, { passive: false })
+    }
+
+    if (doPanning) {
+      this.on('mousedown.panZoom', panStart, this, { passive: false })
+    }
 
     return this
-  }
+  },
 })
